c458e86b09533b3cc3533e061a88cbfcefa184f1
Joe Kozlowski <kozineffect@github.com>
2017-04-24 18:58:04 -0400

    Fixed twitterkeys and spotify calls

--- liri.js
+++ liri.js
@@ -1,13 +1,7 @@
 var request = require('request');
 var Twitter = require('twitter');
 var spotify = require('spotify');
-// var client = require("./keys.js");
-var client = new Twitter({ 
-  consumer_key: 'I78KLgdlrhuQ0vloGTRPg6fk9',
-  consumer_secret: 'KjIDKBHzmRKhDNyfaBgbu3aQXS4gAm1S02co1xD77rhdOn0BJf',
-  access_token_key: '855142132184698880-0Pym3VFWrX9vklfqlVrRwxIz2PB6DaI',
-  access_token_secret: 'lDAShkOiFxX8DDqCK9EUBFBHksuR9S7kTFfBYjuXagx4e'
-});
+var client = require("./keys.js").twitterkeys;
 
 var to_do = process.argv[2];
 var queryArr = [];
@@ -18,12 +12,10 @@ for (var i = 3; i < process.argv.length; i++) {
 }
 var query = queryArr.toString();
 var queryUrl = "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&r=json";
-// console.log(client);
 var params = {screen_name: 'joseph_koz'};
 if (to_do === "my-tweets"){
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets.length);
     for (var x = 0; x < tweets.length; x++){
     console.log(tweets[x].text);
     }
@@ -39,10 +31,13 @@ if (to_do === "spotify-this-song") {
             console.log('Error occurred: ' + err);
             return;
         }
-        console.log(data.tracks.items[0].artist);
+        // console.log(query);
+        console.log(data.tracks.items[0].album.external_urls.spotify);
+        console.log(data.tracks.items[0].album.name);
+        console.log(data.tracks.items[0].album.artists[0].name);
         console.log(query);
-        console.log(data.tracks.items[0].album);
-        console.log(data.tracks.items[0].preview_url);
+
+        // console.log(data.tracks.items[0].preview_url);
         // console.log(JSON.stringify(data, null, 2));
         // Do something with 'data' 
     });
@@ -65,9 +60,6 @@ if (to_do === "movie-this") {
             console.log("The Rotten Tomatoes score is " + JSON.parse(body).Ratings[1].Value);
             console.log("Poster URL " + JSON.parse(body).Poster);
 
-            if (JSON.parse(body).imdbRating < 5) {
-                console.log("This movie sucked!");
-            }
         }
     });
 }
